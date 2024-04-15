34a9245be5ed5df817d0ab75d0da43ff85ac5153
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2024-04-15 02:23:07 +0200

    renamed background image

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,7 +2,7 @@ export const ErrorPage = () => (
   <div className="bg-teal-100">
     <div
       className="h-screen w-screen bg-repeat flex flex-col"
-      style={{ backgroundImage: "url(images/background.svg)" }}
+      style={{ backgroundImage: "url(images/background_pattern.svg)" }}
     >
       <h1 className="text-9xl"> OUPS</h1>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => (
   <div className="bg-teal-100">
     <div
       className="h-screen w-screen bg-repeat flex flex-col"
-      style={{ backgroundImage: "url(images/background.svg)" }}
+      style={{ backgroundImage: "url(images/background_pattern.svg)" }}
     >
       <div className="flex justify-center items-center h-screen">
         <div className="flex p-4  bg-white border border-gray-200 rounded-lg shadow">
